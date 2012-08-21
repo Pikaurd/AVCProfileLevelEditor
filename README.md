f3842c5a49376d31721f83ee2959f2a1b400482a
Pikaurd <pikaurd@gmail.com>
2012-08-21 18:17:17 +0800

    First commit

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ AVCProfileLevelEditor
 =====================
 
 H.264/AVC Profile and Level Editor for PlayStation Vita 
+
+有很多PlayStation® Vita不能播放的MP4可以通过修改H.264/AVC的Profile Level使其
+支持.
+目前已知的Vita认可Profile Level为:
+  Main Profile 2.1 0x4D 40 15
+  Main Profile 3.1 0x4D 40 1f
+  High Profile 3.1 0x64 00 1f
+并且只有同Profile间修改可以支持
+
+使用方法:
+确认有Python 3的运行环境以后, 切换到本程序所在目录(或者设置环境变量), 格式:
+  python3 main.py 文件列表
+  本程序支持一次修改多个文件
+如果操作成共完成原始文件会被修改, 并且生成一个记录文件用来保存原来的Profile Level
+如果修改后的文件不能播放的话使用:
+  python3 main.py -r 要恢复的那个文件路径
+会读取记录文件并恢复成修改前的状态
+
+日志文件格式:
+前三个字节(byte)为原始Profile Level的二进制信息, 第二行是所在文件中位置, 第
+三行是Profile Level的16进制表示
+
+可能存在的问题:
+  1. 因为对MPEG-4文件结构不是很了解, 不知道它的文件头有多长, 所以本程序只读
+  取前8192个字节信息进行修改, 可能有些文件会有问题
+  2. 目前只支持ISO Media, MPEG v4 system, version 1
+  3. 即使是ISO Media, MPEG v4 system, version 1但是没有在前8k找到avcC标识也
+  不被支持
+
+
---
+++ main.py
@@ -0,0 +1,91 @@
+#!/usr/local/bin/python3
+
+import sys
+import os.path
+
+HighProfile = bytes.fromhex('64001f') # High Profile 3.1
+MainProfile = bytes.fromhex('4d401f') # Main Profile 3.1
+MagicCode   = 'avcC'.encode('ascii')
+
+def avcLevelEditor(filePath):
+  try:
+    currentProfile = None
+    with open(filePath, 'r+b') as f:
+      t = f.read(8192)
+      index = t.index(MagicCode)
+      assert(index)
+      currentProfile = t[index+5 : index + 8]
+      if currentProfile == MainProfile and currentProfile == HighProfile:
+        print('skip ' + filePath)
+        return # It is already supported AVC Level
+      if currentProfile[0] == HighProfile[0]:
+        toProfile = HighProfile
+        print("change to High Profile")
+      if currentProfile[0] == MainProfile[0]:
+        toProfile = MainProfile
+        print("change to Main Profile")
+      
+      print(list(map(hex, currentProfile)))
+      #t = t.replace(currentProfile, toProfile)
+      #f.seek(0)
+      #f.write(t)
+
+    # write log file
+    with open(filePath+'.log', 'w') as f:
+      f.write(currentProfile.decode('utf8'))
+      f.write('\nindex: ' + str(index))
+      t = list(map(hex, currentProfile))
+      f.write('\n' + ' '.join(t))
+
+    print('finished')
+  except ValueError as e:
+    print('Sorry, I don\'t support this file\n')
+
+def avcLevelRestore(filePath):
+  try:
+    with open(filePath + '.log', 'rb') as f:
+      level = f.read(3)
+
+    with open(filePath, 'r+b') as f:
+      t = f.read(8192)
+      index = t.index(MagicCode)
+      assert(index)
+      currentProfile = t[index+5 : index + 8]
+      t = t.replace(currentProfile, level)
+      f.seek(0)
+      f.write(t)
+
+    print("AVC Level restore to {}".format([hex(e) for e in level]))
+  except IOError as e:
+    print('Opps… This file has not a restore log')
+
+if __name__ == '__main__':
+  if sys.argv[1] == '-r': # restore mode
+    avcLevelRestore(os.path.abspath(sys.argv[2]))
+  else:
+    for e in sys.argv[1:]:
+      avcLevelEditor(os.path.abspath(e))
+#
+#  if sys.argv[1] == '-b':
+#    # Batch mode
+#    for e in sys.argv[2:]:
+#      avcLevelEditor(os.path.abspath(e))
+#  else:
+#    #print('Single Mode')
+#    avcLevelEditor(os.path.abspath(sys.argv[1]))
+
+def seekSymbol(handle, symbol):
+  f = lambda h, x: h.read(1) == x.encode('utf8')
+  if f(handle, 'a'):
+    if f(handle, 'v'):
+      if f(handle, 'c'):
+        if f(handle, 'C'):
+          return
+
+def h264LevelModify(handle, position):
+  handle.seek(position + 1)     # skip 'avcC' followed one byte
+  currentLevel = handle.read(3) # read 3 byte profile
+  handle.seek(position + 1)
+  handle.write(HighProfile)
+# find another >currentLevel<
+# replace it to >HighProfile<
